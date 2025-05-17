86c7a38c8da98f580923c15c687bc2916101a3da
touki3206 <basketballtouki@icloud.com>
2025-05-17 15:11:38 +0900

    kadai_007
    課題を再提出

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,19 +1,14 @@
-let num = 15;
+let num =  15;
 
-if (num % 3 === 0) {
-  console.log(num + " 3の倍数です。");
-} else {
-  console.log(num);
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log('3と5の倍数です');
+} 
+else if (num % 3 === 0 && num % 5 !== 0) {
+    console.log('3の倍数です');
+} 
+else if (num % 5 === 0 && num % 3 !== 0) {
+    console.log('5の倍数です');
 }
-
-if (num % 5 === 0) {
-  console.log(num + " 5の倍数です。");
-} else {
-  console.log(num);
-}
-
-if (num % 5 === 0 && num % 3 === 0) {
-  console.log(num + " 3と5の倍数です。");
-} else {
-  console.log(num);
+else {
+    console.log(num);
 }
